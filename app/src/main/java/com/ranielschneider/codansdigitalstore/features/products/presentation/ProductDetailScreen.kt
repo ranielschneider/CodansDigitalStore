dd9daada58e4079c6f6b3ddf60de8c1b901aa2db
Raniel Schneider <ranielschneider@hotmail.com>
2026-09-01 21:22:49 +0100

    feat: complete product detail

--- app/src/main/java/com/ranielschneider/codansdigitalstore/features/products/presentation/ProductDetailScreen.kt
+++ app/src/main/java/com/ranielschneider/codansdigitalstore/features/products/presentation/ProductDetailScreen.kt
@@ -1,11 +1,18 @@
 package com.ranielschneider.codansdigitalstore.features.products.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 
 @Composable
 fun ProductDetailScreen(
@@ -22,8 +29,28 @@ fun ProductDetailScreen(
             text = "Carregando..."
         )
     } else {
-        Text(
-            text = product!!.title
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            AsyncImage(
+                model = product!!.thumbnail,
+                contentDescription = product!!.title
+            )
+
+            Text(
+                text = product!!.title
+            )
+
+            Text(
+                text = "€ ${product!!.price}"
+            )
+
+            Text(
+                text = product!!.description
+            )
+        }
     }
 }
